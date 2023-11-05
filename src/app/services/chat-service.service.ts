39f203dc48ed35c67a2036f26fdb567a85ffcc0f
alicanyucel <yucelalican@hotmail.com>
2023-11-06 00:01:53 +0300

    kahve

--- src/app/services/chat-service.service.ts
+++ src/app/services/chat-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@microsoft/signalr';
+import { environment } from 'src/environments/environment.prod';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
 
-  constructor() { }
+  constructor(private _htppClient:HttpClient) 
+  {}
+  registerUser(user:User)
+  {
+    return this._htppClient.post('${environment.apiUrl}api/chat/register-user',user,{responseType:'text'});
+  }
 }
